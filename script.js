ceca9ed096941a106d3930633cc6314ecfdcedbb
nickeee11 <nickechernikov@gmail.com>
2025-04-06 21:16:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const express = require("express");
+const app = express();
+
+const CONFIRMATION_CODE = "f839bac1";
+
+app.use(express.json());
+
+app.post("/callback", (req, res) => {
+    if (req.body.type === "confirmation") {
+        return res.send(CONFIRMATION_CODE); // Отправляем код
+    }
+    
+    // Остальная логика бота...
+    res.send("ok");
+});
+
+app.listen(3000, () => console.log("Сервер запущен!"));
